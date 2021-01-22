0887b20ae4dfaad8037dae717a4069c05c290cda
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-22 16:32:53 -0700

    config

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+// const proxy = require('http-proxy-middleware');
+// const { createProxyMiddleware } = proxy;
 
 const app = express();
 const PORT = 3005;
 
+app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+  console.log(`we are listening on PORT: ${PORT}`);
+});
+
+/* nothing below is being used -- will delete later */
+
 // const API_SERVICE_URL = "http://localhost:3000";
+
+// app.use(createProxyMiddleware('/', {target: 'http://localhost:3001'}));
+// app.use(createProxyMiddleware('/', {target: 'http://localhost:3000'}));
+
+// app.use(proxy('/', {target: 'http://localhost:3003'}));
+
 
 
 // app.use('', (req, res, next) => {
@@ -27,25 +47,18 @@ app.use(morgan('dev'));
 //   },
 // }));
 
-app.use('/', createProxyMiddleware({
-  target: 'http://localhost:3001',
-  changeOrigin: true,
-  pathRewrite: {
-      [`^/localhost:3001`]: '',
-  },
-}));
+// app.use('/', createProxyMiddleware({
+//   target: 'http://localhost:3001',
+//   changeOrigin: true,
+//   pathRewrite: {
+//       [`^/localhost:3001`]: '',
+//   },
+// }));
 
-app.use('/', createProxyMiddleware({
-  target: 'http://localhost:3000',
-  changeOrigin: true,
-  pathRewrite: {
-      [`^/localhost:3000`]: '',
-  },
-}));
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`we are listening on PORT: ${PORT}`);
-})
+// app.use('/', createProxyMiddleware({
+//   target: 'http://localhost:3000',
+//   changeOrigin: true,
+//   pathRewrite: {
+//       [`^/localhost:3000`]: '',
+//   },
+// }));
