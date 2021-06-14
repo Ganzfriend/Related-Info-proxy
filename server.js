b9db8be7d622841bcc5a7ca4e6265f7fe9fbb9fa
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-06-14 15:53:30 +0200

    add static.json file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
